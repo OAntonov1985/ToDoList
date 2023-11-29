283073cc1b45bf98029426165338dd65289efec8
OAntonov1985 <aantonov1985@gmail.com>
2023-11-29 16:01:16 +0200

    29.11

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,29 @@
 // import data from '../data.json'
 
 
-document.querySelector('.button').addEventListener('click', one);
-function one() {
-    fetch('../data.json')
+document.querySelector('.button').addEventListener('click', render);
+let arrToDo = [];
+async function one() {
+    await fetch('../data.json')
         .then((response) => response.json())
-        .then((json) => console.log(json.actualList));
+        .then((json) => arrToDo = json.actualList)
+    console.log(arrToDo)
+
+}
+one()
+
+function render() {
+    const todoListElement = document.getElementById('todoList');
+    arrToDo.map((task) => {
+        const taskElement = document.createElement('ul');
+        taskElement.innerHTML = `
+        <li>${task.Name}</li>
+        <li><strong>Category:</strong> ${task.Category}</li>
+        <li><strong>Created:</strong> ${task.Created}</li>
+        <li><strong>Content:</strong> ${task.Content}</li>
+        <li><strong></strong> ${task.Dates}</li>        
+        <hr>
+    `;
+        todoListElement.appendChild(taskElement);
+    });
 }
